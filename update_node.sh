da6cd527c47d7bd0312c885b6528d74041a5f545
ktjbrowne <ktjbrowne@gmail.com>
2018-08-14 18:35:25 +0100

    Update update_node.sh

--- update_node.sh
+++ update_node.sh
@@ -40,7 +40,7 @@ echo "Upgrading fantasygold..."
 #mkdir ./fantasygold-temp #&& cd ./fantasygold-temp
 rm *
 wget $TARBALLURL
-tar -xzf $TARBALLNAME #&& mv bin fantasygold-$FGCVERSION
+tar -xvzf $TARBALLNAME #&& mv bin fantasygold-$FGCVERSION
 rm $TARBALLNAME
 
 cp -f ./fantasygoldd /usr/local/bin
