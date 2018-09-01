62a200e80ed42d6b9e7c84a627498592f86b27f8
ktjbrowne <ktjbrowne@gmail.com>
2018-09-01 20:53:24 +0100

    asdsa

--- install-multi-mn1.sh
+++ install-multi-mn1.sh
@@ -44,7 +44,7 @@ OUTPUTIDX=''
 # show script Details
 doWelcome(){
   printHead0 "Welcome to the FGC Multi MN Installer for v_1.2.5"
-  echo "V1"
+  echo "V2"
   #read -e -p "Enter your Private Key (genkey):  " MN_KEY
 }
 
