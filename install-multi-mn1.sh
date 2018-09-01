dc5b9f09595636d5663b656d35896bb4110ea442
ktjbrowne <ktjbrowne@gmail.com>
2018-09-01 12:26:22 +0100

    s

--- install-multi-mn1.sh
+++ install-multi-mn1.sh
@@ -42,6 +42,7 @@ OUTPUTIDX=''
 #
 # show script Details
 doWelcome(){
+  echo -e "\e[1;43;30m Yes it is awful \e[0m"
   prettySection "Welcome to the FGC Multi MN Installer for v_1.2.5"
   read -e -p "Enter your Private Key (genkey):  " MN_KEY
 }
