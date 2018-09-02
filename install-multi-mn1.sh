159158079a13ee722eecbb2ac8c65a91b3509a33
ktjbrowne <ktjbrowne@gmail.com>
2018-09-02 13:59:27 +0100

    sadas

--- install-multi-mn1.sh
+++ install-multi-mn1.sh
@@ -44,7 +44,7 @@ OUTPUTIDX=''
 # show script Details
 doWelcome(){
   printHead0 "Welcome to the FGC Multi MN Installer for v_1.2.5"
-  echo "V2"
+  echo "V3"
   #read -e -p "Enter your Private Key (genkey):  " MN_KEY
 }
 
